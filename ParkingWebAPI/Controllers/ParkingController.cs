6187bba556add7507b615b10b55eeef4328e7a8c
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-19 01:06:09 +0300

    Create ParkingController. Fixes #14

--- ParkingWebAPI/Controllers/ParkingController.cs
+++ ParkingWebAPI/Controllers/ParkingController.cs
@@ -2,40 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ParkingWebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Produces("application/json")]
+    [Route("api/Parking")]
+    public class ParkingController : Controller
     {
-        // GET api/values
+        // GET: api/Parking
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        // GET: api/Parking/5
+        [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
+        
+        // POST: api/Parking
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        
+        // PUT: api/Parking/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
